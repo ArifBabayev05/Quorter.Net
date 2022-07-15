7f8ba70c96ecad21a73395275ecc9ab45b983067
ArifBabayev05 <arifrb@code.edu.az>
2022-07-15 04:13:34 +0400

    Create Services,Repos, Partials, ViewComponents,

--- Business/ValidationRules/AboutValidator.cs
+++ Business/ValidationRules/AboutValidator.cs
@@ -11,7 +11,7 @@ namespace Business.ValidationRules
             RuleFor(x => x.Description).NotEmpty().WithMessage("I know you are in hurry, but this field is required.");
             RuleFor(x => x.Image).NotEmpty().WithMessage("Please Do Not Forget Upload Image.");
             RuleFor(x => x.Description).MinimumLength(15).WithMessage("Please use more than 15 charachter for Description");
-            RuleFor(x => x.Description).MaximumLength(500).WithMessage("Please use less than 500 charachter for Description");
+            RuleFor(x => x.Description).MaximumLength(500).WithMessage("Please use less than 500  charachter for Description");
 
         }
     }
